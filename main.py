c6b7325738b02a7d6db7315ed8dec9e5cab7d66e
AMGIRI-1996 <anuragmgiri@gmail.com>
2017-04-23 15:49:51 +0530

    GUI

--- main.py
+++ main.py
@@ -31,14 +31,26 @@ def init():
         file1 = drive.CreateFile({'title': main_dir ,"mimeType": "application/vnd.google-apps.folder"})
         file1.Upload()
             
-def getDirPath() :
+def getDirPath(top) :
     root = tk.Tk()
     root.withdraw()
     dire = filedialog .askdirectory()
 
+    top.pr(dire)
     print(dire)
+
     #return os.path.basename(os.path.normpath(dire))
     return dire
+
+def getFilePath(top) :
+    root = tk.Tk()
+    root.withdraw()
+    file = filedialog.askopenfilename()
+
+    top.pr(file)
+    print(file)
+    #return os.path.basename(os.path.normpath(dire))
+    return file
 
 def getId(tit,d_id):
 	c_id= 0
@@ -49,61 +61,94 @@ def getId(tit,d_id):
 
 	return c_id
 
-def createFolder(tit,d_id):
+def createFolder(top,tit,d_id):
 	file1 = drive.CreateFile({'title': tit,"parents":  [{"id": d_id}],"mimeType": "application/vnd.google-apps.folder"})
 	file1.Upload()
-	print("created folder %s" % tit)
+	top.pr("created folder %s" % tit)
+    print("created folder %s" % tit)
 
-def uploadFile(d,tit,d_id):
+def uploadFile(top,d,tit,d_id):
 	path="%s/%s" % (d,tit)
 	c_id= getId(tit,d_id);
 	if c_id==0 :
 		file1 = drive.CreateFile({'title': tit,"parents":  [{"id": d_id}]})
 		file1.SetContentFile(path)
 		file1.Upload()
-		print("created file %s" % tit)
+		top.pr("created file %s" % tit)
+        print("created file %s" % tit)
 	else:
 		file1 = drive.CreateFile({'id': c_id })
 		file1.Delete()
-		print("deleted file %s" % tit)
-		uploadFile(d,tit,d_id)
+		top.pr("deleted file %s" % tit)
+        print("deleted file %s" % tit)
+		uploadFile(top,d,tit,d_id)
 
-	
-
-
-    
-def uploadFolder( d,d_id):
+def uploadFolder(top, d,d_id):
     tit= os.path.basename(os.path.normpath(d))
     
     c_id= getId(tit,d_id);    
     if c_id==0 :
-        createFolder(tit,d_id)
+        createFolder(top,tit,d_id)
         c_id= getId(tit,d_id);
     else :
-    	print("%s already exixts" % d)
+    	top.pr("%s already exixts" % d)
+        print("%s already exixts" % d)
 
     for name in os.listdir(d):
         if os.path.isfile(os.path.join(d, name)):
         	try:
-        		uploadFile(d,name,c_id)
+        		uploadFile(top,d,name,c_id)
         	except:
-        		print("error in file: %s"%name)
+        		top.pr("error in file: %s"%name)
+                print("error in file: %s"%name)
         	else:
-        		print("is file: %s"%name)
+        		top.pr("is file: %s"%name)
+                print("is file: %s"%name)
 
         elif os.path.isdir(os.path.join(d, name)):
         	pa="%s/%s" % (d,name)
         	try:
         		uploadFolder(pa,c_id)
         	except:
-        		print("Error in Dir: %s"%name)
+        		top.pr("Error in Dir: %s"%name)
+                print("Error in Dir: %s"%name)
         	else:
-        		print("is Dir: %s"%name)
+        		top.pr("is Dir: %s"%name)
+                print("is Dir: %s"%name)
 
 
+    top.pr("%s uploaded" % d)
     print("%s uploaded" % d)
-    
-init()
-D = getDirPath()
-uploadFolder(D,main_dir_id)
-print('title: %s, id: %s , file to upload : %s' % (main_dir, main_dir_id, D))
+
+
+def uploadOnlyFile(top,path,tit,d_id):
+    #path="%s/%s" % (d,tit)
+    c_id= getId(tit,d_id);
+    if c_id==0 :
+        file1 = drive.CreateFile({'title': tit,"parents":  [{"id": d_id}]})
+        file1.SetContentFile(path)
+        file1.Upload()
+        top.pr("created file %s" % tit)
+        print("created file %s" % tit)
+    else:
+        file1 = drive.CreateFile({'id': c_id })
+        file1.Delete()
+        top.pr("deleted file %s" % tit)
+        print("deleted file %s" % tit)
+        uploadOnlyFile(top,path,tit,d_id)
+
+
+def uploadFolderInit(top):
+    global main_dir,main_dir_id
+    init()
+    D = getDirPath(top)
+    uploadFolder(top,D,main_dir_id)
+    #top.pr('title: %s, id: %s , upload : %s' % (main_dir, main_dir_id, D))
+
+def uploadFileInit(top):
+    global main_dir,main_dir_id
+    init()
+    F = getFilePath(top)
+    name = os.path.basename(os.path.normpath(F))
+    uploadOnlyFile(top,F,name,main_dir_id)
+    #top.pr('title: %s, id: %s , file to upload : %s' % (main_dir, main_dir_id, D))
